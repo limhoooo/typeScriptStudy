eda01d1caaf923154714ab632cdc395aeccf9f68
lim ho <dlagh123@gmail.com>
2021-03-30 20:57:35 +0900

    .

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,17 +9,44 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
-function startApp() {
-  axios
-    .get(url)
-    .then(function (response) {
-      console.log(response);
-      user = response.data;
-      // TODO: 이름, 이메일, 주소 표시하기
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+
+/**
+ * 
+ * @typedef {object} User
+ * @property {string} name 
+ * @property {string} email 
+ * @property {Address} address 
+ */
+
+
+/**
+ *
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @returns {Promise<User>} 
+ * 
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.address.city
+})
+
+async function startApp() {
+  const result = await fetchUser()
+  console.log(result);
+  user = result.data;
+  console.log(user);
+  username.innerText = user[0].name
+  email.innerText = user[0].email
+  address.innerText = user[0].address
 }
 
 startApp();
---
+++ why-ts/sample.js
@@ -0,0 +1,17 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// sum()
+
+// @ts-check
+
+/**
+ * 
+ * @param {number} a dsds
+ * @param {number} b dsds
+ */
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20)
