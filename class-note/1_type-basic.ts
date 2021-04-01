03678225f52e27f9747995f5356a685d73269659
limhoooo <dlagh123@gmail.com>
2021-04-01 19:16:29 +0900

    .

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// JS 문자열 선언
+var str = 'hello';
+// TS 문자열 선언
+var str: string = 'hello'
+// TS 숫자열 선언
+let num: number = 10;
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let arr2: Array<string> = ['1', '2', '3'];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ['1', 2];
+
+// TS 객체
+let obj: object = {};
+let person1: object = {
+    name: 'capt',
+    age: 100
+};
+let person2: { name: string, age: number } = {
+    name: 'thor',
+    age: 1000
+}
+
+// TS 진위값
+let show: boolean = true;
