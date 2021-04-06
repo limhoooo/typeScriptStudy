eea9ee6fc4dd88890e509fdf3e3f19335848664e
lim ho <dlagh123@gmail.com>
2021-04-06 19:46:49 +0900

    .

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,20 +3,65 @@ interface User {
     name: string;
 }
 
-let seho: User;
 
+// 변수에 인터페이스 활용
+let seho: User = {
+    age: 33,
+    name: '세호'
+};
 
-seho.age = 1;
-seho.name = '이름';
+// 함수에 인터페이스 활용
+function getUser(user:User) {
+ console.log(user);
+    
+}
+const capt = {
+    name: '캡틴',
+    age: 100
+}
+getUser(capt);
 
-interface Member {
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+    (a: number, b: number): number;
+}
+
+let sum: SumFunction;
+
+sum = function(a:number,b:number): number {
+    return a + b;
+}
+
+// 인덱싱
+interface StringArray {
+    [index:number] : string
+}
+let arr: StringArray = ['a', 'b', 'c'];
+// arr[0] = 10;
+
+// 딕셔너리 패턴
+interface StringRegexDictionay {
+    [key: string]: RegExp;
+}
+
+let obj: StringRegexDictionay = {
+    sth: /abc/,
+    cssFile:/\.css$/,
+}
+
+// 인터페이스 확장
+interface Person {
     name: string;
     age: number;
 }
 
-let test = [];
+interface Developer extends Person {
+    language: string;
+}
 
-test.push({name:'dsads',age:7})
-
-
+let captain: Developer = {
+    name: 'gd',
+    age: 100,
+    language : 'ts'
+}
 
