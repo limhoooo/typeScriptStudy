ac093c7aa824255ec28e509665fc826d41ab071b
lim ho <dlagh123@gmail.com>
2021-04-13 19:55:06 +0900

    .

--- class-note/7_class.ts
+++ class-note/7_class.ts
@@ -21,3 +21,27 @@ class Person3 {
     }
 
 }
+
+class Person2 {
+    private name: string;
+    public age: number;
+    readonly log: string;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
+class Person3 {
+    private name: string;
+    public age: number;
+    readonly log: string;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
