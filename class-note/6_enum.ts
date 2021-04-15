30d3bca26828ef62df6cbfe500c0b9198560edef
lim ho <dlagh123@gmail.com>
2021-04-15 20:24:16 +0900

    .

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -19,6 +19,12 @@ enum Answer {
 function askQuestion(answer: Answer) {
     if (answer === Answer.Yes) {
         console.log('정답입니다.');
+                console.log('정답입니다.');
+
+                        console.log('정답입니다.');
+
+        console.log('정답입니다.');
+
     }
     if (answer === Answer.No) {
         console.log('오답입니다.');
